085f650bfaa90eaac4942400479f3cfaa0bac3e0
Ethan Eldridge <ejayeldridge@gmail.com>
2024-12-08 03:35:06 -0500

    Day 8: Working part 1

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -6,20 +6,27 @@ fn main() {
     let raw_data = raw_data.as_str();    
     let (matrix, rows, cols) = make_matrix(raw_data);
     let antinodes = create_antinodes(matrix, rows, cols);
+    println!("{:?}", antinodes);
     let mut count_of_antinodes = 0;
+    let mut count_of_unique_nodes = 0;
     for row in antinodes {
         for maybe_node in row {
             if maybe_node.is_some() {
                // print!("{:?} ", maybe_node);
+               print!("#");
                count_of_antinodes += 1;
+            } else {
+                print!(".");
             }
-            
+            count_of_unique_nodes += maybe_node.map(|node| node.size()).unwrap_or(0);
         }
-        // println!("")
+        println!("")
     }
-    // your answer it too low:  221
+    // your answer it too low:  221, 344
     // your answer is too high: 456
+    // correct, 376
     println!("{:?}", count_of_antinodes);
+    println!("{:?}", count_of_unique_nodes);
 }
 
 #[derive(Clone, Eq, Hash, PartialEq, Debug)]
@@ -60,6 +67,7 @@ impl AntennaLocation {
         let row_distance = (self.row as isize) - (to_row) as isize;
         let col_distance = (self.col as isize) - (to_col) as isize;
         let distance = row_distance.abs() + col_distance.abs();
+        println!("Distance from {},{} to {},{} is {:?}", self.row, self.col, to_row, to_col, distance);
         distance as usize
     }
     fn new(antenna: Antenna, row: usize, col: usize) -> AntennaLocation {
@@ -75,10 +83,13 @@ impl Antenna {
             frequency
         }
     }
-    fn find_same_frequency(&self, map: &Vec<Vec<Option<Antenna>>>, rows: usize, cols: usize) -> Vec<AntennaLocation> {
+    fn find_same_frequency(&self, map: &Vec<Vec<Option<Antenna>>>, rows: usize, cols: usize, ignore_r: usize, ignore_c: usize) -> Vec<AntennaLocation> {
         let mut friends = Vec::new();
         for row in 0..rows {
             for col in 0..cols {
+                if row == ignore_r && col == ignore_c {
+                    continue;
+                }
                 if let Some(other) = &map[row][col] {
                     if other.frequency == self.frequency {
                         friends.push(
@@ -136,56 +147,61 @@ fn create_antinodes(map: Vec<Vec<Option<Antenna>>>, rows: usize, cols: usize) ->
     for row in 0..rows {
         for col in 0..cols {
             if let Some(antenna) = &map[row][col] {
-                let others = antenna.find_same_frequency(&map, rows, cols);
+                let others = antenna.find_same_frequency(&map, rows, cols, row, col);
+                println!("Antenna {:?}{},{} has same frequency as {:?}", antenna, row, col, others);
                 for other in others {
                     let d = other.distance_to(row,col);
-                    if d % 2  == 0 {
+                    if d > 0 {
                         // an antinode occurs at any point that is perfectly in line 
                         // with two antennas of the same frequency - but only when one
                         // of the antennas is twice as far away as the other
-                        if row + d < rows && col + d < cols {
-                            let location = AntennaLocation::new(
-                                antenna.clone(),
-                                row + d,
-                                col + d
-                            );
-                            match &antinodes[row][col] {
-                                None => {
-                                    antinodes[row][col] = Some(AntiNode::with(location));
-                                }
-                                Some(node) => {
-                                    let known = node.known_locations.clone();
-                                    let mut antinode = AntiNode::with(location);
-                                    for n in known {
-                                        antinode.add_node(n);
-                                    }
-                                    antinodes[row][col] = Some(antinode);
-                                }
-                            }
+                        let row_d = row as isize - other.row as isize;
+                        let col_d = col as isize - other.col as isize;
+
+                        let row_1 = (row as isize + row_d) as usize;
+                        let row_2 = (other.row as isize - row_d) as usize;
+
+                        let col_1 = (col as isize + col_d) as usize;
+                        let col_2 = (other.col as isize - col_d) as usize;
+
+                        if row_1 < rows && col_1 < cols {
+                            println!("making antinode at {:?} {:?} for freq {:?}({},{}) and d {}",  row_1, col_1, antenna.frequency, row, col, d);
+                            antinodes = add_antinode(antenna.clone(), row_1, col_1, antinodes);
                         }
-                        if (row as isize) - d as isize > 0 && (col as isize) - d as isize > 0 {
-                             let location = AntennaLocation::new(
-                                antenna.clone(),
-                                row - d,
-                                col - d
-                            );
-                            match &antinodes[row][col] {
-                                None => {
-                                    antinodes[row][col] = Some(AntiNode::with(location));
-                                }
-                                Some(node) => {
-                                    let known = node.known_locations.clone();
-                                    let mut antinode = AntiNode::with(location);
-                                    for n in known {
-                                        antinode.add_node(n);
-                                    }
-                                    antinodes[row][col] = Some(antinode);
-                                }
-                            }
+                        if row_2 < rows && col_2 < cols {
+                            println!("making antinode at {:?} {:?} for freq {:?}({},{}) and d {}",  row_2, col_2, antenna.frequency, row, col, d);
+                            antinodes = add_antinode(antenna.clone(), row_2, col_2, antinodes);
                         }
                     }
                 }
             }
+        }
+    }
+    antinodes
+}
+
+fn add_antinode(
+    antenna: Antenna,
+    anti_row: usize,
+    anti_col: usize,
+    mut antinodes: Vec<Vec<Option<AntiNode>>>
+) -> Vec<Vec<Option<AntiNode>>> {
+    let location = AntennaLocation::new(
+        antenna,
+        anti_row,
+        anti_col
+    );
+    match &antinodes[anti_row][anti_col] {
+        None => {
+            antinodes[anti_row][anti_col] = Some(AntiNode::with(location));
+        }
+        Some(node) => {
+            let known = node.known_locations.clone();
+            let mut antinode = AntiNode::with(location);
+            for n in known {
+                antinode.add_node(n);
+            }
+            antinodes[anti_row][anti_col] = Some(antinode);
         }
     }
     antinodes
