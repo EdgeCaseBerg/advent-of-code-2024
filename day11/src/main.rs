682eaa68dedef95b531c5048189450afa8e4ec90
Ethan Eldridge <ejayeldridge@gmail.com>
2024-12-10 23:35:06 -0500

    Boilerplate for reading a file into a string

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::error::Error;
+use std::env;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let maybe_filename = get_filename_from_args();
+    if maybe_filename.is_none() {
+        return Err("No file provided".into());
+    }
+    let message: String = fs::read_to_string(maybe_filename.unwrap())?;
+    println!("{}", message);
+    Ok(())
+}
+
+
+fn get_filename_from_args() -> Option<String> {
+    let arguments: Vec<String> = env::args().collect();
+    if arguments.is_empty() {
+        println!("No filename passed to file. Defaulting to sample.txt");
+        return None;
+    }
+    let mut arguments = arguments.iter();
+    arguments.next(); // skip the name of the program being ran
+    arguments.next().cloned()
 }
