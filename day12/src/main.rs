12bec15f9b5df79e42e2993cf10f906f9133ba6f
Ethan Eldridge <ejayeldridge@gmail.com>
2024-12-12 02:33:03 -0500

    Day 12 part 1 complete!

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -6,14 +6,19 @@ use std::collections::VecDeque;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // let maybe_filename = get_filename_from_args();
-    let maybe_filename = Some(String::from("../sample1.txt"));
+    let maybe_filename = Some(String::from("../input.txt"));
     if maybe_filename.is_none() {
         return Err("No file provided".into());
     }
     let input: String = fs::read_to_string(maybe_filename.unwrap())?;
     let plants = make_matrix(&input);
     let regions = find_regions(&plants);
-    println!("{:?}", regions);
+    let mut cost = 0;
+    for region in regions {
+        println!("{} {}", region.planted.value, region.perimeter());
+        cost += region.price();
+    }
+    println!("Cost {:?}", cost);
     Ok(())
 }
 
@@ -32,8 +37,7 @@ impl Plant {
 struct Region {
     planted: Plant,
     plants: Vec<(usize, usize)>,
-    adjacent: HashSet<(usize, usize)>,
-    regions_within_this: Vec<Region>
+    touching: HashSet<(isize, isize)>
 }
 
 impl Region {
@@ -48,8 +52,7 @@ impl Region {
         Region {
             planted: plant,
             plants: Vec::new(),
-            regions_within_this: Vec::new(),
-            adjacent: HashSet::new()
+            touching: HashSet::new()
         }
     }
 
@@ -57,16 +60,43 @@ impl Region {
         self.plants.push(plot);
     }
 
-    fn add_region(&mut self, region: Region) {
-        self.regions_within_this.push(region);
-    }
-
-    fn add_boundary_plant(&mut self, at: (usize, usize)) {
-        self.adjacent.insert(at);
+    fn add_boundary_plant(&mut self, at: (isize, isize)) {
+        self.touching.insert(at);
     }
 
     fn perimeter(&self) -> u64 {
-        0
+        if self.plants.len() == 1 {
+            return 4;
+        }
+        let mut p = 0;
+        let directions = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+        for plot in self.plants.iter() {
+            let mut exposed_edges = 0;
+            for (d_row, d_col) in &directions {
+                let new_row = plot.0 as isize + d_row;
+                let new_col = plot.1 as isize + d_col;
+                if self.touching.contains(&(new_row, new_col)) {
+                    exposed_edges += 1;
+                }
+            }
+            p += exposed_edges;
+        }
+        if p == 0 {
+            println!("{:?}", self);
+        }
+        p
+    }
+
+    fn surrounded(&self, point: (usize, usize)) -> bool {
+        let min = self.plants.iter().min_by_key(|(r,_)| r).unwrap();
+        let max = self.plants.iter().max_by_key(|(r,_)| r).unwrap();
+        let within_x = min.0 < point.0 && point.0 < max.0;
+        
+        let min = self.plants.iter().min_by_key(|(_, c)| c).unwrap();
+        let max = self.plants.iter().max_by_key(|(c,_)| c).unwrap();
+        let within_y = min.1 < point.1 && point.1 < max.1;
+        
+        within_x && within_y
     }
 }
 
@@ -125,9 +155,12 @@ fn bfs(plots: &Vec<Vec<Plant>>, of_type: &Plant, start_row: usize, start_col: us
 
     let directions = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 
+    let within_bounds = |row: isize, col: isize| -> bool {
+        row >= 0 && col >= 0 && (row as usize) < rows && (col as usize) < cols
+    };
+
     let is_same_plant = |row: isize, col: isize| -> bool {
-        let within_bounds = row >= 0 && col >= 0 && (row as usize) < rows && (col as usize) < cols;
-        if !within_bounds {
+        if !within_bounds(row, col) {
             return false;
         }
         let plant_here = plots[row as usize][col as usize];
@@ -138,7 +171,6 @@ fn bfs(plots: &Vec<Vec<Plant>>, of_type: &Plant, start_row: usize, start_col: us
     let mut queue = VecDeque::new();
     let mut visited = HashSet::new();
     queue.push_back((start_row, start_col));
-    region.add_plot((start_row, start_col));
 
     while let Some((row, col)) = queue.pop_front() {
         for (d_row, d_col) in &directions {
@@ -150,10 +182,17 @@ fn bfs(plots: &Vec<Vec<Plant>>, of_type: &Plant, start_row: usize, start_col: us
                 visited.insert((new_row, new_col));
                 queue.push_back((row, col));
                 region.add_plot((row, col));
-            } else {
-                region.add_boundary_plant((row,col));
+            } else if within_bounds(new_row, new_col) && !is_same_plant(new_row, new_col) {
+                region.add_boundary_plant((new_row, new_col));
+            } else if !within_bounds(new_row, new_col) {
+                region.add_boundary_plant((new_row, new_col));
             }
         }
     }
+
+    if region.plants.len() == 0 {
+        region.add_plot((start_row, start_col));
+    }
+
     region
 }
