302d1e6b1309d7a66431d21239ce9e91717e9ae4
Ethan Eldridge <ejayeldridge@gmail.com>
2024-12-14 23:19:22 -0500

    Fiddling around with crates and modules some more

---
+++ day15/src/boilerplate.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::fs;
+
+fn get_filename_from_args() -> Option<String> {
+    let arguments: Vec<String> = env::args().skip(1).collect();
+    if arguments.is_empty() {
+        return None;
+    }
+    let mut arguments = arguments.iter();
+    arguments.next().cloned()
+}
+
+pub fn get_sample_if_no_input() -> Result<String, std::io::Error> {
+    match get_filename_from_args() {
+        None => fs::read_to_string("sample.txt"),
+        Some(filename) => fs::read_to_string(filename)
+    }
+}
--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,3 +1,11 @@
+pub mod boilerplate;
+
+
 fn main() {
-    println!("Hello, world!");
+    let raw_data = crate::boilerplate::get_sample_if_no_input();
+    if let Err(ref problem) = raw_data {
+        println!("Could not read input data: {:?}", problem);
+        return;
+    }
+    println!("{:?}", raw_data);
 }
