bfa38dd8fef2632fd7e10ec3293975177f5359c7
Ethan Eldridge <ejayeldridge@gmail.com>
2024-12-17 07:05:25 -0500

    Day 17 Add boilerplate + initial parse of registers

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,3 +1,42 @@
+pub mod boilerplate;
+
+// use std::collections::VecDeque;
+
 fn main() {
-    println!("Hello, world!");
+    let raw_data = crate::boilerplate::get_sample_if_no_input();
+    if let Err(ref problem) = raw_data {
+        println!("Could not read input data: {:?}", problem);
+        return;
+    }
+    let data = raw_data.unwrap();
+    part_1(&data);
+    // part_2(&data);
+}
+
+fn part_1(data: &str) {
+    let (a, b, c) = parse_initial_state(data);
+    let computer_state = ThreeBitComputer {
+        reg_A: a,
+        reg_B: b,
+        reg_C: c
+    };
+
+    println!("{:?}", computer_state);
+}
+
+fn parse_initial_state(data: &str) -> (RegisterInteger, RegisterInteger, RegisterInteger) {
+    let mut line_iter = data.lines().take_while(|line| !line.is_empty()).take(3);
+    let a: RegisterInteger = line_iter.next().unwrap().split(":").nth(1).unwrap().trim().parse().unwrap();
+    let b: RegisterInteger = line_iter.next().unwrap().split(":").nth(1).unwrap().trim().parse().unwrap();
+    let c: RegisterInteger = line_iter.next().unwrap().split(":").nth(1).unwrap().trim().parse().unwrap();
+    (a, b, c)
+}
+
+type RegisterInteger = i64;
+
+#[derive(Debug)]
+struct ThreeBitComputer {
+    reg_A: RegisterInteger,
+    reg_B: RegisterInteger,
+    reg_C: RegisterInteger
 }
