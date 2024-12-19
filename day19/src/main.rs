970ba7e2567ed83857776a11ad8ee90ddb4cfdaf
Ethan Eldridge <ejayeldridge@gmail.com>
2024-12-19 00:56:59 -0500

    Day 19 Part 1 first attempt
    Is it just taking a while or is it infinite looping? I'm not sure.
    It worked on the sample input so it seems like the idea to convert each
    provided pattern into a small parsing engine, then check the input
    against each parser, consuming the input matched so far then checking
    each potentialy parser against that worked in a small scale.
    
    But I suppose this is going to be a bit slow on a larger input. I could
    probably use the RegEx library and it would be significantly faster, but
    that would be passing up a chance to write a little parser which is no
    fun.

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -12,10 +12,126 @@ fn main() {
 }
 
 fn part_1(data: &str) {
-    let _foo = data;
+    let raw_designs = data
+        .lines()
+        .take_while(|line| !line.is_empty())
+        .map(|line| line.split(", ").collect::<Vec<&str>>())
+        .flatten()
+        .collect::<Vec<&str>>();
+    let parsers = raw_designs.iter().map(|raw| {
+        let design_tokens = raw.chars().filter_map(|c| TowelStripe::from(c)).collect::<Vec<TowelStripe>>();
+        Design {
+            design: design_tokens
+        }
+    }).collect::<Vec<Design>>();
+
+    let request_designs = data
+        .lines()
+            .skip_while(|line| !line.is_empty()).skip(1)
+            .map(|raw| {
+                raw.chars().filter_map(|c| TowelStripe::from(c)).collect::<Vec<TowelStripe>>()
+            })
+            .collect::<Vec<Vec<TowelStripe>>>();
+    
+    let mut valid_designs = 0;
+    for design in &request_designs {
+        let mut leftover = design.clone();
+        if try_parsers(&parsers, &leftover) {
+            valid_designs += 1;
+        }
+    }
+    println!("Valid designs {:?}", valid_designs);
+}
+
+fn try_parsers(parsers: &Vec<Design>, input: &[TowelStripe]) -> bool {
+    let mut valid_parsers = vec![];
+    for parser in parsers {
+        if let Some(to_consume) = parser.matches(input) {
+            // Fully matched?
+            if to_consume == input.len() {
+                return true;
+            }
+
+            // Deepen the search for this parse
+            let eventually_worked = try_parsers(parsers, &input[to_consume..]);
+            if eventually_worked {
+                valid_parsers.push(parser);
+            }
+        }
+    }
+    return valid_parsers.len() > 0;
 }
 
 
 fn part_2(data: &str) {
     let _foo = data;
 }
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+enum TowelStripe {
+    White,
+    Blue,
+    Black,
+    Red,
+    Green
+}
+impl TowelStripe {
+    fn from(c: char) -> Option<TowelStripe> {
+        match c {
+            'w' | 'u' | 'b' | 'r' | 'g' => Some(TowelStripe::of(c)),
+            _ => None
+        }
+    }
+
+    fn of(c: char) -> TowelStripe {
+        match c {
+            'w' => TowelStripe::White,
+            'u' => TowelStripe::Blue,
+            'b' => TowelStripe::Black,
+            'r' => TowelStripe::Red,
+            'g' => TowelStripe::Green,
+            _ => panic!("Invalid character given for towel")
+        }
+    }
+
+    fn is_value(&self, value: &char) -> bool {
+        if let Some(stripe) = TowelStripe::from(*value) {
+            *self == stripe
+        } else {
+            false
+        }
+    }    
+}
+
+#[derive(Debug)]
+struct Design {
+    design: Vec<TowelStripe>,
+}
+
+impl Design {
+    fn matches(&self, against: &[TowelStripe]) -> Option<usize> {
+        let num_tokens = against.len();
+        let num_chars = self.design.len();
+        let mut chars = self.design.clone().into_iter();
+        let mut matching = Vec::new();
+
+        if num_chars > against.len() {
+            return None;
+        }
+
+        for idx in 0..num_chars.min(num_tokens) {
+            match chars.next() {
+                None => return None,
+                Some(char_to_match) => {
+                    if against[idx] == char_to_match {
+                        matching.push(char_to_match.clone())
+                    } else {
+                        return None
+                    }
+                }
+            }
+        }
+        return Some(matching.len());
+    }
+}
+
