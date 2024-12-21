cfe9bb14488708e52ff4a32f3341e6bb42c4b8a3
Ethan Eldridge <ejayeldridge@gmail.com>
2024-12-21 16:31:57 -0500

    Writing in moves from each button
    The input is small and I'm so so so tired of writing for dir in
    direction and doing bounds checks with as isize as usize and I just want
    to do something else please

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,5 +1,8 @@
 pub mod boilerplate;
 
+use std::collections::VecDeque;
+use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn main() {
     let raw_data = crate::boilerplate::get_sample_if_no_input();
@@ -34,9 +37,22 @@ fn get_numeric_of(code: &str) -> u64 {
     code.chars().take_while(|c| c.is_digit(10)).fold(String::new(), |accum, c| accum + &c.to_string()).parse().unwrap()
 }
 
+type Position = (usize, usize);
+#[derive(Debug)]
+enum Action {
+    Up,
+    Down,
+    Left,
+    Right,
+    Press
+}
+
+use Action::*;
+
 struct KeyPad {
     buttons: Vec<Vec<char>>,
-    position: (usize, usize)
+    position: Position,
+    neighbors: HashMap<char, Vec<(char, Action)>>
 }
 
 impl KeyPad {
@@ -49,8 +65,25 @@ impl KeyPad {
                 vec!['1', '2', '3'],
                 vec![' ', '0', 'A']
             ],
-            position: (3, 2)
+            position: (3, 2),
+            neighbors: HashMap::from([
+                ('9', vec![ ('8', Left ), ('6', Down)                             ]),
+                ('8', vec![ ('7', Left ), ('9', Right), ('5', Down)               ]),
+                ('7', vec![ ('4', Down ), ('8', Right)                            ]),
+                ('6', vec![ ('9', Up   ), ('5', Left ), ('3', Down)               ]),
+                ('5', vec![ ('8', Up   ), ('4', Left ), ('6', Right), ('2', Down) ]),
+                ('4', vec![ ('7', Up   ), ('5', Right), ('1', Down)               ]),
+                ('3', vec![ ('6', Up   ), ('2', Left ), ('A', Down)               ]),
+                ('2', vec![ ('5', Up   ), ('1', Left ), ('3', Right), ('0', Down) ]),
+                ('1', vec![ ('4', Up   ), ('2', Right)                            ]),
+                ('0', vec![ ('2', Up   ), ('A', Right)                            ]),
+                ('A', vec![ ('0', Left ), ('3', Up   )                            ])
+            ])
         }
+    }
+
+    fn action_of(from: char, to: char) -> Vec<Position> {
+        vec![]
     }
 
     fn directional_keypad() -> Self {
@@ -59,9 +92,27 @@ impl KeyPad {
                 vec![' ', '^', 'A'],
                 vec!['<', 'v', '>']
             ],
-            position: (0, 2)
+            position: (0, 2),
+            neighbors: HashMap::from([
+                ('^', vec![ ('v', Up   ), ('A', Right)               ]),
+                ('A', vec![ ('^', Left ), ('<', Down )               ]),
+                ('>', vec![ ('v', Right), ('A', Up   )               ]),
+                ('v', vec![ ('^', Up,  ), ('<', Left ), ('>', Right) ]),
+                ('<', vec![ ('v', Right)                             ]),
+            ])
         }
-    } 
+    }
+
+    fn is_valid_move(&self, to_pos: (isize, isize)) -> bool {
+        if self.buttons.len() < to_pos.0 as usize && 0 > to_pos.0 {
+            return false;
+        }
+        if self.buttons[0].len() < to_pos.1 as usize && 0 > to_pos.1 {
+            return false;
+        }
+
+        self.buttons[to_pos.0 as usize][to_pos.1 as usize] == ' '
+    }
 }
 
 fn get_presses(target: &str, keypad: &KeyPad) -> String {
