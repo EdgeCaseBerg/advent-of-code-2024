f1c18ff51fa0b5d0725cf925a6f23d10b446f4e8
Ethan Eldridge <ejayeldridge@gmail.com>
2024-12-20 19:41:38 -0500

    Day 20 Part 2 checkpoint before stream
    Sigh. I thought I was close, but maybe not so much. I think the
    manhatten distance is the right way to figure this out but I assume I
    migth be counting incorrectly

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -17,7 +17,7 @@ fn main() {
 }
 
 fn part_1(data: &str) {
-    let (matrix, start_pos, end_pos) = parse_data_to_graph(data);
+    let (matrix, start_pos, _) = parse_data_to_graph(data);
     // Part 1 isn't asking for cheats along the best path, it's asking for ANY cheat that saves 100 seconds.
     // ANYWHERE.
     // So instead of djikstra, just BFS from the start and fill up each path bit with a count of how many
@@ -32,7 +32,7 @@ fn part_1(data: &str) {
     let mut step_counts_by_zone = Vec::new();
     for row in 0..matrix.len() {
         let mut cols = Vec::new();
-        for col in 0..matrix[row].len() {
+        for _ in 0..matrix[row].len() {
             cols.push(i64::MAX);
         }
         step_counts_by_zone.push(cols);
@@ -121,8 +121,135 @@ fn part_1(data: &str) {
 }
 
 fn part_2(data: &str) {
-    let _foo = data;
+    // Ok this sucks. But... let's do the same thing as before, where
+    // we build up an initial cost matrix that tells us much we save
+    // from moving from point x to poiny y.
+    let (matrix, start_pos, _) = parse_data_to_graph(data);
+        let in_bounds = |row: usize, col: usize| -> bool {
+        let within_row = 0 <= row && row < matrix.len();
+        if !within_row {
+            return false;
+        }
+        let within_col = 0 <= col && col < matrix[row].len();
+        within_row && within_col
+    };
+    let direction: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+    let mut step_counts_by_zone = Vec::new();
+    for row in 0..matrix.len() {
+        let mut cols = Vec::new();
+        for _ in 0..matrix[row].len() {
+            cols.push(i64::MAX); // <--- this will probably be a bit screwy.
+        }
+        step_counts_by_zone.push(cols);
+    }
+
+    let mut queue = VecDeque::new();
+    let mut visited = HashSet::new();
+    let mut distance_to_get_to: i64 = 0;
+    let at_least_this_much = 100;
+    queue.push_back(start_pos);
+    step_counts_by_zone[start_pos.0][start_pos.1] = 0;
+    while let Some(current_pos) = queue.pop_front() {
+        if visited.contains(&current_pos) {
+            continue;
+        }
+
+        visited.insert(current_pos);
+        distance_to_get_to += 1;
+
+        for dir in &direction {
+            let next_row = (current_pos.0 as isize + dir.0) as usize;
+            let next_col = (current_pos.1 as isize + dir.1) as usize;
+            if !in_bounds(next_row, next_col) {
+                continue;
+            }
+
+            if matrix[next_row][next_col] == NodeType::Wall {
+                continue;
+            }
+
+            if !visited.contains(&(next_row, next_col)) {
+                step_counts_by_zone[next_row][next_col] = distance_to_get_to;
+            }
+            queue.push_front((next_row, next_col));
+        }
+    }
+
+    // Now, we need to explore the path _again_ (DFS style?), but this time. We need to cheat.
+    // So traverse the path again, but this time, don't skip when we see a wall (that isn't out of bounds)
+    // and instead allow the path to be explored up to 20 times. If the path we are cheating to
+    // is a valid place to end up (even less than 20, then add it as a potential cheat)
+    let mut cheats_by_position: HashMap<(Position, Position), i64> = HashMap::new() ;
+    let mut cheats_by_position_and_time: HashMap<(Position, Position, i64), i64> = HashMap::new(); // for debugging.
+    let mut queue = VecDeque::new();
+    queue.push_back(start_pos);
+    let mut visited = HashSet::new();
+    while let Some(current_pos) = queue.pop_back() {
+        if visited.contains(&current_pos) {
+            continue;
+        }
+        visited.insert(current_pos);
+        let (row, col) = current_pos;
+
+        let steps_to_position = step_counts_by_zone[row][col];
+        for dir in &direction {
+            let next_row = (current_pos.0 as isize + dir.0) as usize;
+            let next_col = (current_pos.1 as isize + dir.1) as usize;
+            if !in_bounds(next_row, next_col) {
+                continue;
+            }
+
+            if matrix[next_row][next_col] == NodeType::Wall {
+                // YOU CAN CHEAT TO EVERYTTHING WITHIN 20 MANHATTANS AWAY
+                // SO ITERATE ACROSS ALL OF THOSE TO FIGURE OUT WHAT ONE 
+                // CAN CHEAT TO
+                for offset_r in -20..=20i64 {
+                    for offset_c in -20..=20i64 {
+                        let manhatten = offset_r.abs() + offset_c.abs();
+                        if manhatten > 20 {
+                            continue;
+                        }
+
+                        let next_row = (current_pos.0 as isize + offset_r as isize) as usize;
+                        let next_col = (current_pos.1 as isize + offset_c as isize) as usize;
+                        if !in_bounds(next_row, next_col) {
+                            continue
+                        }
+
+                        let time_saved_by_cheat = step_counts_by_zone[next_row][next_col];
+                        let diff_in_steps = (steps_to_position - time_saved_by_cheat).abs() - 2;
+
+                        if diff_in_steps.abs() - 2 >= at_least_this_much {
+                            cheats_by_position.entry(((row,col), (next_row, next_col))).and_modify(|c| *c += 1).or_insert(1);
+                            cheats_by_position_and_time.entry( ((row,col), (next_row, next_col), diff_in_steps) ).and_modify(|c| *c += 1).or_insert(1);
+                        }
+                    }
+                }
+                continue;
+            }
+            queue.push_front((next_row, next_col));
+        }
+    }
+
+    println!("{:?}", cheats_by_position);
+    println!("{:?}", cheats_by_position_and_time);
+
+    let mut answer = 0;
+    for ((start, end), number) in cheats_by_position {
+        answer += number;
+    }
+
+    let mut answer_two = 0;
+    for ((start, end, cost_savings), number) in cheats_by_position_and_time {
+        if cost_savings >= 100 {
+            answer_two += number
+        }
+    }
+    // 23544 is too low
+    println!("{:?}", answer);
+    println!("{:?}", answer_two); // Just confirming it is the same
 }
+
 
 type Matrix = Vec<Vec<NodeType>>;
 type Position = (usize, usize);
@@ -135,40 +262,7 @@ enum NodeType {
     Wall
 }
 
-fn apply_cheat(matrix: &Matrix, from_position: Position) -> Vec<Matrix> {
-    let direction: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
-    let in_bounds = |row: usize, col: usize| -> bool {
-        let within_row = row < matrix.len();
-        let within_col = col < matrix[row].len();
-        within_row && within_col
-    };
 
-    let mut matrices = vec![];
-    for dir in &direction {
-        let cheat_row_start = (from_position.0 as isize + dir.0) as usize;
-        let cheat_col_start = (from_position.1 as isize + dir.1) as usize;
-
-        let cheat_row_end = (from_position.0 as isize + dir.0 * 2) as usize;
-        let cheat_col_end = (from_position.1 as isize + dir.1 * 2) as usize;
-
-        if !in_bounds(cheat_row_start, cheat_row_end) || !in_bounds(cheat_row_end, cheat_col_end) {
-            continue;
-        }
-
-        if matrix[cheat_row_start][cheat_col_start] != NodeType::Wall {
-            continue;
-        }
-
-        if matrix[cheat_row_end][cheat_col_end] != NodeType::Path && matrix[cheat_row_end][cheat_col_end] != NodeType::End {
-            continue;
-        }
-
-        let mut cheat_worldview = matrix.clone();
-        cheat_worldview[cheat_row_start][cheat_col_start] = NodeType::Path;
-        matrices.push(cheat_worldview);
-    }
-    matrices
-}
 
 fn parse_data_to_graph(data: &str) -> (Matrix, Position, Position) {
     let matrix = data.lines().map(|line| {
@@ -202,98 +296,3 @@ fn parse_data_to_graph(data: &str) -> (Matrix, Position, Position) {
     
     (graph, start_node, end_node)
 }
-
-// #[derive(Debug)]
-// struct DijkstraResult {
-//     distances: HashMap<Position, usize>, // Distance to each position
-//     prev: HashMap<Position, Option<Position>>, // Previous node in the path
-//     cheats_beating_min_time: u64,
-// }
-
-// fn dijkstra(matrix: &Matrix, start: Position, end: Position, cheat_position: Option<Position>, time_to_beat: u64) -> Option<DijkstraResult> {
-//     let rows = matrix.len();
-//     let cols = matrix[0].len();
-    
-//     let mut distances: HashMap<Position, usize> = HashMap::new();
-//     let mut prev: HashMap<Position, Option<Position>> = HashMap::new();
-//     let mut heap = BinaryHeap::new(); // Priority queue, use `Reverse` for a min-heap
-    
-//     // Initialize distances and the heap
-//     for r in 0..rows {
-//         for c in 0..cols {
-//             let pos = (r, c);
-//             distances.insert(pos, usize::MAX);
-//             prev.insert(pos, None);
-//         }
-//     }
-    
-//     // Start position distance is 0
-//     distances.insert(start, 0);
-//     heap.push(Reverse((0, start))); // Push (distance, position) into the heap
-    
-//     // Helper function to get neighbors
-//     let neighbors = |pos: Position| -> Vec<Position> {
-//         let (r, c) = pos;
-//         let mut result = vec![];
-//         if r > 0 { result.push((r - 1, c)); } // Up
-//         if r < rows - 1 { result.push((r + 1, c)); } // Down
-//         if c > 0 { result.push((r, c - 1)); } // Left
-//         if c < cols - 1 { result.push((r, c + 1)); } // Right
-//         result
-//     };
-    
-//     // Main loop
-//     while let Some(Reverse((current_distance, current_position))) = heap.pop() {
-//         // If we reach the end, stop
-//         if current_position == end {
-//             break;
-//         }
-        
-//         // Skip if this distance is outdated
-//         if current_distance > distances[&current_position] {
-//             continue;
-//         }
-
-
-//         if cheat_position.is_some() && cheat_position.unwrap() == current_position {
-//             let must_save_at_least = 100;
-//             let new_matrices = apply_cheat(&matrix, current_position);
-//             let mut number_of_cheats_saving_time_at_this_position = 0;
-//             if new_matrices.len() > 0 {
-//                 for cheated_matrix in new_matrices {
-//                     let cheat_result = dijkstra(&cheated_matrix, start, end, None, time_to_beat).unwrap();
-//                     let time = cheat_result.distances[&end];
-//                     if (time as u64) < time_to_beat && time_to_beat - time as u64 <= must_save_at_least {
-//                         println!("save {:?}", time_to_beat - time as u64);
-//                         number_of_cheats_saving_time_at_this_position += 1
-//                     }
-//                 }
-//             }
-//             return Some(DijkstraResult { distances, prev, cheats_beating_min_time: number_of_cheats_saving_time_at_this_position });
-//         }
-        
-//         // Explore neighbors
-//         for neighbor in neighbors(current_position) {
-//             if let NodeType::Wall = matrix[neighbor.0][neighbor.1] {
-//                 continue; // Skip walls
-//             }
-            
-//             let tentative_distance = current_distance + 1; // Distance to the neighbor
-            
-//             // If this path is shorter, update it
-//             if tentative_distance < distances[&neighbor] {
-//                 distances.insert(neighbor, tentative_distance);
-//                 prev.insert(neighbor, Some(current_position));
-//                 heap.push(Reverse((tentative_distance, neighbor)));
-//             }
-//         }
-//     }
-    
-//     // If we never reach the end, return None
-//     if distances[&end] == usize::MAX {
-//         return None;
-//     }
-    
-//     Some(DijkstraResult { distances, prev, cheats_beating_min_time: 0 })
-// }
-
