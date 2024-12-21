ec3ec13e50668b761e8efa72e40c8ed9ef4e8503
Ethan Eldridge <ejayeldridge@gmail.com>
2024-12-21 02:05:40 -0500

    Day 21 copy over boilerplate
    I'm just going to assume it's going to be a graph problem again

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,3 +1,67 @@
+pub mod boilerplate;
+
+
 fn main() {
-    println!("Hello, world!");
+    let raw_data = crate::boilerplate::get_sample_if_no_input();
+    if let Err(ref problem) = raw_data {
+        println!("Could not read input data: {:?}", problem);
+        return;
+    }
+    let data = raw_data.unwrap();
+    part_1(&data);
+    part_2(&data);
+}
+
+fn part_1(data: &str) {
+    // let (matrix, start_pos, _) = parse_data_to_graph(data);
+    let _foo = data;
+}
+
+fn part_2(data: &str) {
+    let _foo = data;
+}
+
+
+type Matrix = Vec<Vec<NodeType>>;
+type Position = (usize, usize);
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+enum NodeType {
+    Start,
+    End,
+    Path,
+    Wall
+}
+
+fn parse_data_to_graph(data: &str) -> (Matrix, Position, Position) {
+    let matrix = data.lines().map(|line| {
+        line.chars().map(|char_in_line| {
+            match char_in_line {
+                '.' => NodeType::Path,
+                'S' => NodeType::Start,
+                'E' => NodeType::End,
+                _   => NodeType::Wall,
+            }
+        }).collect::<Vec<NodeType>>()
+    }).collect::<Vec<Vec<NodeType>>>();
+
+    let mut graph = Vec::new();
+    let mut start_node = (0,0);
+    let mut end_node = (0,0);
+    for (row, cols) in matrix.iter().enumerate() {
+        let mut r = Vec::new();
+        for (col, &node) in cols.iter().enumerate() {
+            if node == NodeType::Start {
+                start_node = (row, col);
+            }
+
+            if node == NodeType::End {
+                end_node = (row, col);
+            }
+            r.push(matrix[row][col]);
+        }
+        graph.push(r);
+    }
+    
+    (graph, start_node, end_node)
 }
