281f8128a98e19f6d80176e0a83bdc238c987906
Ethan Eldridge <ejayeldridge@gmail.com>
2024-12-23 09:48:35 -0500

    D23P2 pre-sleeping musings for posterity

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -55,11 +55,70 @@ fn part_1(data: &str) {
         }
     }
     println!("{:?}", might_be_chief_historian_count);
-    
 }
 
 fn part_2(data: &str) {
-    let _foo = data;
+    let list_of_connections = parse_network_map_from(data);
+    let mut connected_map = HashMap::new();
+    for connection in &list_of_connections {
+        let set = connected_map.entry(&connection.from).or_insert(HashSet::new());
+        set.insert(connection.to.clone());
+        let set = connected_map.entry(&connection.to).or_insert(HashSet::new());
+        set.insert(connection.from.clone());
+    }
+
+    // Observation. Every Set has the same size! Is this information useful in some way?
+    // 
+    
+    // let mut largest = HashSet::new();
+    for (&pc1, p1_connected_to) in &connected_map {
+        // Part 2 doesn't say this implicitly, but can I assume that t must be in the input?
+        if !pc1.starts_with("t") {
+            continue;
+        }
+
+        let mut connected = vec![];
+        for pc in p1_connected_to {
+            let one_hop = connected_map.get(pc).unwrap();
+            println!("oo{:?}", {p1_connected_to.difference(one_hop)});
+            let mut key_and_set = one_hop.clone();
+            key_and_set.insert(pc.clone());
+            connected.push(key_and_set);
+        }
+
+        // We have a list of the other connections now.
+        let mut best_connected_so_far = vec![];
+        for pc in p1_connected_to {
+            let mut remaining = vec![];
+            for connection_set in &connected {
+                if connection_set.contains(pc) {
+                    remaining.push(connection_set.clone());
+                }
+            }
+            if best_connected_so_far.len() < remaining.len() {
+                best_connected_so_far = remaining.clone();
+            }
+            connected = remaining;
+        }
+        println!("c{:?}", connected);
+        println!("b{:?}", best_connected_so_far);
+
+        // if largest.len() < best_connected_so_far.len() {
+        //     let mut x = best_connected_so_far[0];
+        //     for i in 1..best_connected_so_far.len() {
+        //         x = x.intersection(&best_connected_so_far[i]).collect()
+        //     }
+        //     largest = x;
+        // }
+    }
+
+    // "co": {"ta", "ka", "de", "tc"},
+    // "ta": {"ka", "co", "de", "kh"}
+    // "ka": {"ta", "co", "tb", "de"}
+    // "de": {"ta", "cg", "co", "ka"}
+    // "kh": {"ub", "ta", "tc", "qp"}
+
+    println!("{:?}", connected_map);
 }
 
 #[derive(Debug)]
