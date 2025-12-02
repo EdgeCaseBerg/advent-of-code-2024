7ad004800b395428955a81d2693e581a2d17856d
peetseater <peetseater@gmail.com>
2025-12-02 00:58:39 -0500

    day 2 part 1 complete
    writing some real garbage rust

--- 2025/day02/src/main.rs
+++ 2025/day02/src/main.rs
@@ -1,3 +1,51 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let raw_data = fs::read_to_string("./input").expect("bad input data");
+    let raw_data = raw_data.as_str();
+    let mut result = 0;
+    for item in raw_data.trim().split(",") {
+        // get the numbers, the problem says no leading 0s but 
+        // just in case, be ready for a string if we need it I guess.
+        let mut iter = item.split("-");
+        let first_str = iter.next().unwrap();
+        let first_num: usize = first_str.parse().expect("bad number 1");
+
+        let last_str = iter.next().unwrap();
+        let last_num: usize = last_str.parse().expect("bad number 2");
+
+        for id in first_num..=last_num {
+            if is_invalid(&id.to_string()) {
+                result += id
+            }
+        }
+    }
+    println!("{:?}", result);
+}
+
+fn is_invalid(id: &str) -> bool {
+    if (id.len() % 2 == 1) {
+        return false
+    }
+    let window_size = id.len()/2;
+    let chars: Vec<char> = id.to_string().chars().collect();
+    let mut chunk_iter = chars.chunks(window_size);
+    let mut seen = 0;
+    let mut left = chunk_iter.next();
+    let mut right = chunk_iter.next();
+    if right.is_none() {
+        return false;
+    }
+        if left.unwrap()[0] == '0' || right.unwrap()[0] == '0' || left.unwrap().len() != right.unwrap().len() {
+            return false
+        }
+        if left.unwrap() == right.unwrap() {
+            seen += 1
+        }
+
+    // we saw 1010, but not 101010
+    if seen == 1 {
+        return true
+    }
+    return false
 }
