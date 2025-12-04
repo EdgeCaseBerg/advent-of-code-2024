93800bc32ffa7bde524784d34052b76fc244cda5
peetseater <peetseater@gmail.com>
2025-12-04 00:00:19 -0500

    Initialize D4P1

---
+++ 2025/day04/src/main.rs
@@ -0,0 +1,12 @@
+use std::fs;
+
+fn main() {
+    let raw_data = fs::read_to_string("./input").expect("bad input data");
+    let raw_data = raw_data.as_str();
+    p1(raw_data);
+    p2(raw_data);
+}
+
+fn p1(_input: &str) {}
+
+fn p2(_input: &str) {}
