3b767508a9319dc0333ac06b63b76c931f51d16d
peetseater <peetseater@gmail.com>
2025-12-05 07:05:27 -0500

    D5P2 checkpoint: still too tired to sort through it

--- 2025/day05/src/main.rs
+++ 2025/day05/src/main.rs
@@ -35,6 +35,86 @@ fn p1(raw_data: &str) {
     println!("{:?}", fresh_count);
 }
 
-fn p2(_raw_data: &str) {
+fn p2(raw_data: &str) {
+    let rules: Vec<Range> = raw_data.lines()
+        .take_while(|line| !line.is_empty())
+        .map(|line| {
+            let mut raw_range = line.split("-");
+            let low: usize = raw_range.next().expect("couldnt read low").parse().expect("couldnt convert low");
+            let high: usize = raw_range.next().expect("couldnt read high").parse().expect("couldnt convert high");
+            Range { low, high }
+        }).collect();
 
+    let mut ranges: Vec<Range> = vec![];
+    ranges.push(Range { low: rules[0].low, high: rules[0].high });
+
+    let mut the_rules = rules.clone();
+    while let Some(rule) = the_rules.pop() {
+        for existing_range in ranges.clone() {
+            if existing_range.contains_whole_range(&rule) {
+                continue;
+            }
+
+            if existing_range.contains_low(&rule) {
+                println!("contains_low {:?} {:?}", existing_range, rule);
+                println!("before\n{:?}", ranges);
+                ranges.retain(|r| r.not_equals(&existing_range));
+
+                ranges.push(existing_range.expand_higher(&rule));
+                println!("after\n{:?}\n", ranges);
+                continue;
+            }
+
+            if existing_range.contains_high(&rule) {
+                println!("contains_high {:?} {:?}", existing_range, rule);
+                ranges.retain(|r| r.not_equals(&existing_range));
+                ranges.push(existing_range.expand_lower(&rule));
+                continue;
+            }
+
+            // It's new
+            ranges.push(rule.clone());
+        }
+        println!("{:?}", ranges);
+    }
+    ranges.sort();
+    ranges.dedup();
+    
+    let mut ids_in_ranges = 0;
+    for range in ranges {
+        ids_in_ranges += range.high - range.low + 1;
+    }
+    println!("{:?}", ids_in_ranges);
+    
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Ord, Eq, PartialOrd)]
+struct Range {
+    low: usize,
+    high: usize
+}
+
+impl Range {
+    fn contains_whole_range(&self, other: &Range) -> bool {
+        self.contains_low(other) && self.contains_high(other)
+    }
+
+    fn contains_low(&self, other: &Range) -> bool {
+        self.low <= other.low && other.low <= self.high
+    }
+    fn contains_high(&self, other: &Range) -> bool {
+        self.low <= other.high && other.high <= self.high
+    }
+
+    fn expand_higher(&self, other: &Range) -> Range {
+        Range { low: self.low, high: other.high }
+    }
+
+    fn expand_lower(&self, other: &Range) -> Range {
+        Range { low: other.low, high: self.high }
+    }
+
+    fn not_equals(&self, other: &Range) -> bool {
+        !(self.low == other.low && self.high == other.high)
+    }
 }
